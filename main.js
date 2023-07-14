77e5c0c646621b7414372af9eb81f86f03a32325
Phạm Tuyên <phamthanhtuyen2k8@gmail.com>
2023-07-14 21:31:48 +0700

    hate

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 const path = require("path")
 const gtts = require("node-gtts")("vi")
-const {execSync} = require('child_process')
+const {execSync, spawn} = require('child_process')
 const internetAvailable = require("internet-available")
 /*
 function internetOn() {
@@ -17,6 +17,19 @@ function internetOn() {
     return on
 }
 */
+let play = spawn('node', ['./play.js']);
+
+play.stdout.on('data', function (data) {
+  console.log('stdout: ' + data.toString());
+});
+
+play.stderr.on('data', function (data) {
+  console.log('stderr: ' + data.toString());
+});
+
+play.on('exit', function (code) {
+  console.log('child process exited with code ' + code.toString());
+});
 let config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf8"))
 
 function countFolder() {
@@ -145,51 +158,3 @@ for(let i = 0; i < config.length; i++) {
     console.log(`process ${i} spawned`)
     downloadProcess(i)
 }
-
-function play(i) {
-    if (config[i].type === "text") {
-        for (let index = 0; index < 3; index++) {
-            execSync(`mpg123 ${path.join(__dirname, config[i].dir.toString(), "0.mp3")}`)
-        }
-    } else if (config[i].type === "link") {
-        execSync(`mpg123 ${path.join(__dirname, config[i].dir.toString(), `${config[i].index}.mp3`)}`)
-        config[i].index += 1
-        let count = 0
-        fs.readdirSync(config[i].dir.toString()).forEach(item => {
-            if (item.endsWith(".mp3")) count += 1
-        })
-        if (config[i].index === count) config[i].index = 0
-        fs.writeFileSync(path.join(__dirname, "config.json"), JSON.stringify(config))
-        config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf8"))
-    }
-}
-
-function playProcess(time, day, i) {
-    let d = new Date()
-    let sec = d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds()
-    let endDay = 24 * 3600
-    let delay
-    if (time >= sec) delay = (time - sec) * 1000; else delay = (endDay - sec + time) * 1000
-    console.log(`set delay ${delay}`)
-    console.log(`play process ${i}`)
-    console.log(`play at ${(Math.floor(delay / 3600000)).toString()}:${(Math.floor(((delay / 3600000) * 60) % 60)).toString()}`)
-    setTimeout(() => {
-        let wd = new Date()
-        let wday = (wd.getDay() == 0 ? 7 : wd.getDay()) - 1
-        console.log("play")
-        if (day.includes(wday)) {
-            play(i)
-            console.log("play ok")
-        }
-        console.log("play fin")
-        playProcess(time, day, i)
-    }, delay)
-}
-
-for(let i = 0; i < config.length; i++) {
-    config[i].tm.forEach(tm => playProcess(tm.time * 3600, tm.day, i))
-}
-
-
-
-
---
+++ play.js
@@ -0,0 +1,52 @@
+const fs = require("fs")
+const path = require("path")
+const {execSync} = require('child_process')
+
+let config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf8"))
+function play(i) {
+    if (config[i].type === "text") {
+        for (let index = 0; index < 3; index++) {
+            execSync(`mpg123 ${path.join(__dirname, config[i].dir.toString(), "0.mp3")}`)
+        }
+    } else if (config[i].type === "link") {
+        execSync(`mpg123 ${path.join(__dirname, config[i].dir.toString(), `${config[i].index}.mp3`)}`)
+        config[i].index += 1
+        let count = 0
+        fs.readdirSync(config[i].dir.toString()).forEach(item => {
+            if (item.endsWith(".mp3")) count += 1
+        })
+        if (config[i].index === count) config[i].index = 0
+        fs.writeFileSync(path.join(__dirname, "config.json"), JSON.stringify(config))
+        config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf8"))
+    }
+}
+
+function playProcess(time, day, i) {
+    let d = new Date()
+    let sec = d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds()
+    let endDay = 24 * 3600
+    let delay
+    if (time >= sec) delay = (time - sec) * 1000; else delay = (endDay - sec + time) * 1000
+    console.log(`set delay ${delay}`)
+    console.log(`play process ${i}`)
+    console.log(`play at ${(Math.floor(delay / 3600000)).toString()}:${(Math.floor(((delay / 3600000) * 60) % 60)).toString()}`)
+    setTimeout(() => {
+        let wd = new Date()
+        let wday = (wd.getDay() == 0 ? 7 : wd.getDay()) - 1
+        console.log("play")
+        if (day.includes(wday)) {
+            play(i)
+            console.log("play ok")
+        }
+        console.log("play fin")
+        playProcess(time, day, i)
+    }, delay)
+}
+
+for(let i = 0; i < config.length; i++) {
+    config[i].tm.forEach(tm => playProcess(tm.time * 3600, tm.day, i))
+}
+
+
+
+
